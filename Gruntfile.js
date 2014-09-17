7186430c0078f6f0f732386620cf698814fbcad4
P. Smith <patrismith@gmail.com>
2014-09-17 17:17:54 -0400

    added grunt-pngmin task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,11 @@ var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
- 
+
 module.exports = function (grunt) {
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
- 
+
   grunt.initConfig({
     watch: {
       scripts: {
@@ -70,10 +70,24 @@ module.exports = function (grunt) {
         src: ['game/main.js'],
         dest: 'dist/js/game.js'
       }
-    }
+    },
+      pngmin: {
+          dist: {
+              options: {
+                  ext: '.png',
+                  force: true
+              },
+              files: [{
+                  expand: true,
+                  cwd: 'assets',
+                  src: '**/*.png',
+                  dest: 'assets'
+              }]
+          }
+      }
   });
-  
-  grunt.registerTask('build', ['buildBootstrapper', 'browserify','copy']);
+
+  grunt.registerTask('build', ['buildBootstrapper', 'browserify', 'pngmin:dist', 'copy']);
   grunt.registerTask('serve', ['build', 'connect:livereload', 'open', 'watch']);
   grunt.registerTask('default', ['serve']);
   grunt.registerTask('prod', ['build', 'copy']);
